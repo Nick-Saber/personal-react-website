efc8ed0f031e8b9e2e31cf2984477fe885d58124
Nick-Saber <nsaber@berkeley.edu>
2020-02-13 22:00:57 -0800

    change orientation and center all elements, also add random fact tab for time being

--- src/client/components/index.jsx
+++ src/client/components/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AboutMeView from "src/client/components/views/aboutMe";
-import {Tabs, Tab} from '@material-ui/core';
+import {Tabs, Tab, Avatar} from '@material-ui/core';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { MyPhoto } from 'src/client/static/images/image_urls'
 
+const randomFacts = [
+    'A bolt of lightning contains enough energy to toast 100,000 slices of bread',
+    'The average person will spend six months of their life waiting for red lights to turn green',
+    'You can hear a blue whale\'s heartbeat from two miles away',
+    'Nearly 30,000 rubber ducks were lost a sea in 1992 and are still being discovered today',
+    'Sears used to sell houses.',
+    'Cap\'N\'Crunch\'s full name is Horatio Magellan Crunch',
+    'Continental plates drift at about the same rate as fingernails grow',
+    'The scientific term for brain freeze is “sphenopalatine ganglioneuralgia”.'
+]
 
 class LandingPage extends React.Component {
     constructor(props) {
@@ -19,13 +30,11 @@ class LandingPage extends React.Component {
         var selectedTab = this.state.tabSelected
         switch (selectedTab) {
             case 'AboutMeView':
-                return <AboutMeView/>
+                return (<AboutMeView/>)
             case 'Misc':
-                return (
-                    <div>
-                        BLah Blah Blah
-                    </div>
-                )
+                return <div style={{margin: '10px', display: 'flex', "justify-content": "center"}}>
+                    {randomFacts[ Math.floor(Math.random() * randomFacts.length)]}
+                </div>
             default:
                 return "ERRROROROR"
         }
@@ -37,22 +46,27 @@ class LandingPage extends React.Component {
         return (
             <MuiThemeProvider>
             <div>
-                <div>
+                <h1 style={{margin: '5px', display:'flex', "justify-content":"center"}}> Nicholas Reza Saber </h1>
+
+                <div style={{margin: '5px', display:'flex', "justify-content":"center"}} >
+                    <Avatar style ={{width:'50%', height:'50%'}} alt="Me trying to look professionally into the camera" src={MyPhoto}/>
+                </div>
+                <div style={{margin: '5px', display:'flex', "justify-content":"center"}}>
                     <Tabs value={this.state.tabSelected}
-                          onChange={(tab)=>{this.setState({tabSelected: tab})}}
-                    >
+                          onChange={(event, newTabValue)=>{this.setState({tabSelected: newTabValue})}}>
                         <Tab value={'AboutMeView'}
                              label={'About Me'}
-                             style={{defaultTab:{'color': '#ffe2bf', backgroundColor:'#FFFBF5'}}}>
+                             style={{margin: '5px', defaultTab:{'color': '#ffe2bf', backgroundColor:'#FFFBF5'}}}>
                         </Tab>
                         <Tab value={'Misc'}
-                             label={'Random'}
-                             style={{defaultTab:{'color': '#ffe2bf', backgroundColor:'#FFFBF5'}}}>
+                             label={'Random Fact'}
+                             style={{margin: '5px', defaultTab:{'color': '#ffe2bf', backgroundColor:'#FFFBF5'}}}>
                         </Tab>
                     </Tabs>
                 </div>
+
                 <div>
-                {this.renderTabChoice()}
+                    {this.renderTabChoice()}
                 </div>
             </div>
             </MuiThemeProvider>
--- src/client/components/views/aboutMe/index.jsx
+++ src/client/components/views/aboutMe/index.jsx
@@ -1,7 +1,9 @@
-import { MyPhoto } from 'src/client/static/images/image_urls'
 import ResizeImage from 'react-resize-image'
-import React from "react";
+import { MyPhoto } from 'src/client/static/images/image_urls'
+
 import { Avatar } from '@material-ui/core';
+
+import React from "react";
 /*<ResizeImage src={MyPhoto} style={{width:"30%", 'border-radius': "75%"}} alt="Angels Landing - It was the scariest hike of my life!"/>*/
 
 
@@ -12,11 +14,7 @@ class AboutMeView extends React.Component {
     }
     render() {
         return (<div>
-            <div style={{display:"flex", "justify-content":"center"}} >
-                <Avatar  alt="Me trying to look professionally into the camera" src={MyPhoto}/>
-            </div>
             <div style={{display:"flex", "justify-content":"center", "align-items":"center", "flex-direction":"column"}}>
-                <h1 style={{display:'block'}}> Nicholas Reza Saber </h1>
                 <p style={{display:'block'}}> Hi there and welcome to my website! This site is bare bones and still being worked on!</p>
             </div>
         </div>)
